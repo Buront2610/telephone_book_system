0f64549d2d6389de8bf0bcbd64a2aeba8b9c2ade
K-b <bridgefield738@gmail.com>
2023-08-24 14:55:44 +0900

    update main file

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,10 @@ class MyApp extends StatelessWidget {
       title: 'Phone Directory App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: TextTheme(
+          headline5: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          subtitle1: TextStyle(fontSize: 18.0, fontStyle: FontStyle.italic),
+        ),
       ),
       home: MyHomePage(),
     );
@@ -24,60 +28,74 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<Employee> currentEmployees = [];
+  int? selectedTeamIndex;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Phone Directory App'),
+        title: Text('電話番号一覧'),
       ),
       body: Row(
         children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: Container(
-              color: Colors.blue[100],
-              child: ListView.builder(
-                itemCount: departments.length,
-                itemBuilder: (context, index) {
-                  return ExpansionTile(
-                    title: Text(departments[index].name),
-                    children: departments[index].groups.map((group) => _buildGroupTile(group)).toList(),
-                  );
-                },
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              color: Colors.white,
-              child: ListView.builder(
-                itemCount: currentEmployees.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(currentEmployees[index].name),
-                    subtitle: Text('Extension: ${currentEmployees[index].extension}'),
-                  );
-                },
-              ),
-            ),
-          ),
+          _buildSideBar(),
+          _buildEmployeeList(),
         ],
       ),
     );
   }
 
-  Widget _buildGroupTile(Group group) {
-    return ExpansionTile(
-      title: Text(group.name),
-      children: group.teams.map((team) => _buildTeamTile(team)).toList(),
+  Widget _buildSideBar() {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        color: Colors.blue[100],
+        child: ListView.builder(
+          itemCount: departments.length,
+          itemBuilder: (context, index) {
+            return _buildExpansionTile(departments[index].name, departments[index].groups.map((group) => _buildGroupTile(group)).toList(), Icons.business);
+          },
+        ),
+      ),
     );
+  }
+
+  Widget _buildEmployeeList() {
+    return Expanded(
+      flex: 2,
+      child: Container(
+        color: Colors.white,
+        child: ListView.builder(
+          itemCount: currentEmployees.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Icon(Icons.person),
+              title: Text(currentEmployees[index].name, style: Theme.of(context).textTheme.headline5),
+              subtitle: Text('Extension: ${currentEmployees[index].extension}', style: Theme.of(context).textTheme.subtitle1),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildExpansionTile(String title, List<Widget> children, IconData icon) {
+    return ExpansionTile(
+      leading: Icon(icon),
+      title: Text(title),
+      children: children,
+    );
+  }
+
+  Widget _buildGroupTile(Group group) {
+    return _buildExpansionTile(group.name, group.teams.map((team) => _buildTeamTile(team)).toList(), Icons.group);
   }
 
   Widget _buildTeamTile(Team team) {
     return ListTile(
+      leading: Icon(Icons.group),
       title: Text(team.name),
+      selected: currentEmployees == team.employees,
       onTap: () {
         setState(() {
           currentEmployees = team.employees;
